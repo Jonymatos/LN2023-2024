49cc8a24a37ec10c098e8fadb11c9968617d203c
Jonymatos <joao.rui.matos@tecnico.ulisboa.pt>
2023-10-01 12:11:47 +0100

    Add tests with members tests for FSTs

--- run.sh
+++ run.sh
@@ -12,8 +12,13 @@ done
 
 # ############ CORE OF THE PROJECT  ############
 
+#mix2numerical
 fstconcat compiled/mmm2mm.fst compiled/aux.fst > compiled/mix2numerical.fst
+
+#pt2en
 fstconcat compiled/pt2en_aux.fst compiled/aux.fst > compiled/pt2en.fst
+
+#en2pt
 fstinvert compiled/pt2en.fst > compiled/en2pt.fst
 
 # datenum2text
@@ -35,13 +40,6 @@ fstunion compiled/mix2text1.fst compiled/mix2text2.fst > compiled/mix2text.fst
 fstunion compiled/mix2text.fst compiled/datenum2text.fst > compiled/date2text.fst
 
 
-
-
-
-
-
-
-
 # ############ generate PDFs  ############
 echo "Starting to generate PDFs"
 for i in compiled/*.fst; do
@@ -54,59 +52,75 @@ fst2word() {
 }
 
 #4 - tests the transducers with the students' 18th birthday
-dateMembers=("DEC/02/2018" "MAY/08/2019")
+dateMembersMixPT=("DEZ/02/2018" "MAI/08/2019" "DEZ/2/2018" "MAI/8/2019")
+dateMembersMixEN=("DEC/02/2018" "MAY/08/2019" "DEC/2/2018" "MAY/8/2019")
+dateMembersNumerical=("12/02/2018" "05/08/2019" "12/2/2018" "5/8/2019")
 
 #Testing with syms.txt as output
-FSTs=(mix2numerical.fst en2pt.fst)
+FSTs=(mix2numerical.fst)
 for i in "${FSTs[@]}"; do
     echo "Testing $i:"
-    for w in $dateMembers; do
+    for w in "${dateMembersMixPT[@]}" "${dateMembersMixEN[@]}"; do
         res=$(python3 ./scripts/word2fst.py $w | fstcompile --isymbols=syms.txt --osymbols=syms.txt | fstarcsort |
                            fstcompose - compiled/$i | fstshortestpath | fstproject --project_type=output |
                            fstrmepsilon | fsttopsort | fstprint --acceptor --isymbols=syms.txt | fst2word)
         echo "$w = $res"
     done
+echo "**********************"
 done
 
-FSTs=(day.fst month.fst)
+FSTs=(en2pt.fst)
 for i in "${FSTs[@]}"; do
     echo "Testing $i:"
-    for w in "1" "02" "03" "12"; do
+    for w in ${dateMembersMixEN[@]}; do
         res=$(python3 ./scripts/word2fst.py $w | fstcompile --isymbols=syms.txt --osymbols=syms.txt | fstarcsort |
                            fstcompose - compiled/$i | fstshortestpath | fstproject --project_type=output |
-                           fstrmepsilon | fsttopsort | fstprint --acceptor --isymbols=./scripts/syms-out.txt | fst2word)
+                           fstrmepsilon | fsttopsort | fstprint --acceptor --isymbols=syms.txt | fst2word)
         echo "$w = $res"
     done
+echo "**********************"
 done
 
 FSTs=(datenum2text.fst)
 for i in "${FSTs[@]}"; do
     echo "Testing $i:"
-    for w in "09/15/2055" "12/2/2018" "05/08/2019"; do
+    for w in ${dateMembersNumerical[@]}; do
         res=$(python3 ./scripts/word2fst.py $w | fstcompile --isymbols=syms.txt --osymbols=syms.txt | fstarcsort |
                            fstcompose - compiled/$i | fstshortestpath | fstproject --project_type=output |
                            fstrmepsilon | fsttopsort | fstprint --acceptor --isymbols=./scripts/syms-out.txt | fst2word)
         echo "$w = $res"
     done
+echo "**********************"
 done
 
 FSTs=(mix2text.fst)
 for i in "${FSTs[@]}"; do
     echo "Testing $i:"
-    for w in "MAI/15/2055" "MAY/15/2055"; do
+    for w in "${dateMembersMixPT[@]}" "${dateMembersMixEN[@]}"; do
         res=$(python3 ./scripts/word2fst.py $w | fstcompile --isymbols=syms.txt --osymbols=syms.txt | fstarcsort |
                            fstcompose - compiled/$i | fstshortestpath | fstproject --project_type=output |
                            fstrmepsilon | fsttopsort | fstprint --acceptor --isymbols=./scripts/syms-out.txt | fst2word)
         echo "$w = $res"
     done
+echo "**********************"
+done
+
+FSTs=(date2text.fst)
+for i in "${FSTs[@]}"; do
+    echo "Testing $i:"
+    for w in "${dateMembersMixPT[@]}" "${dateMembersMixEN[@]}" "${dateMembersNumerical[@]}"; do
+        res=$(python3 ./scripts/word2fst.py $w | fstcompile --isymbols=syms.txt --osymbols=syms.txt | fstarcsort |
+                           fstcompose - compiled/$i | fstshortestpath | fstproject --project_type=output |
+                           fstrmepsilon | fsttopsort | fstprint --acceptor --isymbols=./scripts/syms-out.txt | fst2word)
+        echo "$w = $res"
+    done
+echo "**********************"
 done
 
 #TODO just to test date2text when it is ready
 
 #1 - generates files
-echo "\n***********************************************************"
-echo "Testing 4 (the output is a transducer: fst and pdf)"
-echo "***********************************************************"
+echo "Generating files"
 for w in compiled/t-*.fst; do
     fstcompose $w compiled/date2text.fst | fstshortestpath | fstproject --project_type=output |
                   fstrmepsilon | fsttopsort > compiled/$(basename $w ".fst")-out.fst
