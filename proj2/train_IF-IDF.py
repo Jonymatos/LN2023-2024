187e34df860ae8d3a7bb24a536aab03a2b914afa
Jonymatos <joao.rui.matos@tecnico.ulisboa.pt>
2023-10-22 19:43:04 +0100

    Add regrex to remove hotels

--- proj2/train_IF-IDF.py
+++ proj2/train_IF-IDF.py
@@ -5,9 +5,12 @@ from sklearn.pipeline import Pipeline
 from sklearn.metrics import accuracy_score
 from nltk.stem import WordNetLemmatizer
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
-import nltk
-import sys
-from custom_stop_words import chicago_hotel_names
+import nltk, re
+from sklearn.model_selection import train_test_split
+from custom_stop_words import chicago_hotel_names_regrex
+from sklearn.svm import SVC
+
+ROUNDS = 100
 
 df = pd.read_csv("train.txt", sep="\t", names=["label", "review"])
 
@@ -24,54 +27,40 @@ stop_words.remove("but")
 stop_words.remove("only")
 stop_words.remove("no")
 stop_words.remove("against")
+stop_words.append("chicago")
+stop_words.append("hotel")
 
 
 def remove_stop_words(text):
-    return " ".join([word for word in str(text).split() if word.lower() not in stop_words and word.lower() not in chicago_hotel_names])
+    return " ".join([word for word in str(text).split() if word.lower() not in stop_words])
 
 def remove_hotels(text):
-    #See words two by two
-    words = text.split()
-    new_words = []
-    for i in range(len(words)-1):
-        if words[i] + " " + words[i+1] not in chicago_hotel_names:
-            new_words.append(words[i])
-    return " ".join(new_words)
+    for hotel_name in chicago_hotel_names_regrex:
+        text = re.sub(hotel_name, "", text)
+    return text
 
+df.review = df.review.apply(remove_hotels)
 df.review = df.review.apply(remove_stop_words)
-inv_mapping[df["label"][6]], df["review"][6]
 
-from sklearn.model_selection import train_test_split
-
-X_train, X_test, y_train, y_test = train_test_split(df.review, df.label, test_size=0.1, shuffle=True)
+LRC = ("clf", LogisticRegression(solver="liblinear", multi_class="auto"))
+SVC = ("clf", SVC(kernel="linear", gamma="auto"))
 
 clf = Pipeline([
-    ("vect", CountVectorizer()),
+    ("vect", CountVectorizer(lowercase=True)),
     ("tfidf", TfidfTransformer()),
-    ("clf", LogisticRegression())
+    LRC
 ])
 
-clf.fit(X_train, y_train)
-y_pred = clf.predict(X_test)
-#Get review that were wrongly classified
-wrongly_classified = X_test[y_pred != y_test]
+accuracy_scores = []
 
-classifier_accuracy = accuracy_score(y_test, y_pred)
-print("Accuracy of classifier: {:.4f}".format(classifier_accuracy))
+for i in range(ROUNDS):
+    X_train, X_test, y_train, y_test = train_test_split(df.review, df.label, test_size=0.1, shuffle=True, random_state=i)
+    clf.fit(X_train, y_train)
+    y_pred = clf.predict(X_test)
+    clf_accuracy = accuracy_score(y_test, y_pred)
+    accuracy_scores.append(clf_accuracy)
+    #print("Round: ", i, " Accuracy: ", clf_accuracy)
 
-#Test the following review
-review = "The staff was very rude and unhelpful. The room was dirty and the bathroom was not clean. I will not stay here again."
-
-#Predict the sentiment
-sentiment = clf.predict([review])
-print("Sentiment test review: {}".format(inv_mapping[sentiment[0]]))
-print()
-
-for wrongly_classified_review in wrongly_classified:
-    sentiment = clf.predict([wrongly_classified_review])
-    print("Sentiment: {}".format(inv_mapping[sentiment[0]]))
-    print()
-    print("Review: {}".format(wrongly_classified_review))
-    print()
+print("Accuracy: ", sum(accuracy_scores) / len(accuracy_scores))
 
 
